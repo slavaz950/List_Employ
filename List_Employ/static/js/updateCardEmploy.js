35387bfcbf5b08944b3895ce89e2193d815209d5
vyacheslav zamaraev <viacheslav.zamaraev@yandex.ru>
2026-02-10 22:54:41 +0500

    EditEmploy

--- List_Employ/static/js/updateCardEmploy.js
+++ List_Employ/static/js/updateCardEmploy.js
@@ -5,25 +5,64 @@
 
 
 
-editEmploy()
+//document.getElementById("formUpdateEmploy").addEventListener('onclick', function(e) {
+   // e.preventDefault();
 
 async function editEmploy() {
-  const form = document.getElementById('UpdateEmployForm');  // Находим форму на html странице
-  const formData = new FormData(form);                        // Собираем значения из всех полей ввода
-  console.log(formData)
+  
+ // }) 
 
   const pathParts = window.location.pathname.split('/');  // В скобках указываем разделитель (что считать окончанием каждой из частей)
-  const Id = pathParts[3];   // Получаем идентификатор записи
-  const Url = 'api/employee/Id/'
-  response = await fetch(Url, {
+  const id = pathParts[3];   // Получаем идентификатор записи
+  //const Url =
+  const APIUrl = 'api/employee/' + id + '/';
+
+
+
+const form = document.getElementById("formUpdateEmploy");  // Находим форму на html странице
+  console.log(form)
+  const formData = new FormData(form);                        // Собираем значения из всех полей ввода
+
+
+// Проверка содержимого FormData (Для отладки)
+console.log('Содержимое FormData:');
+for (let [key, value] of formData.entries()) {
+  console.log(`${key}: ${value}`);
+}
+
+
+  console.log(formData)
+console.log(APIUrl)
+
+
+////try{
+ const response = await fetch(APIUrl, {  //await
     method:"PUT",
-    //headers: {'Content-Type': 'application/json'},
-    body:JSON.stringify(formData)   // FormData автоматически устанавливает Content-Type
-  });  // Запрашиваем данные на сервере
+    headers: {'Content-Type': 'application/json'  // БЕЗ ЭТОЙ СТРОКИ ОШИБКУ ВЫДАЁТ. Возможно потому что FormData работает не как нужно.
+    },  
+  // body:JSON.stringify(formData)   // FormData автоматически устанавливает Content-Type
+   body:JSON.stringify(Object.fromEntries(formData))
+ }) ;
+
+ if (response.ok) {
+ // alert('Данные обновлены');
+  location.href = '/employlist/';
+ //} else {
+ // throw new Error('Ошибка сервера: ${response.statusText}');
+  }
+
+
+
+
+
+/*
 
 if(response.status === 200) {   // Если обновление данных сотрудника прошло успешно   
   location.href = '/employlist/';  //"window.location.href='/employlist/'"
 }else{  
    }
   
- };
+*/
+
+
+};
