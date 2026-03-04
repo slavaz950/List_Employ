9371244144986ce59ea75909c319ca35b213a88a
vyacheslav zamaraev <viacheslav.zamaraev@yandex.ru>
2026-03-04 19:37:36 +0500

    debug_8

--- deploy.sh
+++ deploy.sh
@@ -100,7 +100,9 @@ install_dependencies() {
             #     sudo rm /etc/apt/sources.list.d/debian.list
 
             # Создаем новый файл sources.list с репозиториями Debian
-            sudo cat << EOF > /etc/apt/sources.list   # << EOF - многострочный ввод в файл /etc/apt/sources.list
+             # sudo cat << EOF > /etc/apt/sources.list   # << EOF - многострочный ввод в файл /etc/apt/sources.list
+            # sudo bash -c     запускает новую оболочку с правами root, и вся команда (включая перенаправление >) выполняется в ней.
+            sudo bash -c 'cat > /etc/apt/sources.list' << EOF
             deb http://deb.debian.org/debian/ stretch main
             deb http://deb.debian.org/debian/ stretch-updates main
             deb http://security.debian.org/debian-security/ stretch/updates main
@@ -131,7 +133,9 @@ EOF
              sudo rm /etc/apt/sources.list.d/debian.list
 
             # Создаем новый файл sources.list с репозиториями Debian
-            sudo cat << EOF > /etc/apt/sources.list   # << EOF - многострочный ввод в файл /etc/apt/sources.list
+            # sudo bash -c cat << EOF > /etc/apt/sources.list   # << EOF - многострочный ввод в файл /etc/apt/sources.list
+            # sudo bash -c     запускает новую оболочку с правами root, и вся команда (включая перенаправление >) выполняется в ней.
+            sudo bash -c 'cat > /etc/apt/sources.list' << EOF
             deb http://deb.debian.org/debian buster main contrib non-free
             deb http://deb.debian.org/debian buster-updates main contrib non-free
             deb http://security.debian.org/debian-security buster/updates main contrib non-free
